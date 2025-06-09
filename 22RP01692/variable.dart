8e082fd34747f8ef0481e2d5f0c9bb084ace5026
olivieririho <irihoolivier2002@gmail.com>
2025-06-09 15:43:28 -0800

    task commit

---
+++ 22RP01692/variable.dart
@@ -0,0 +1,11 @@
+void main() {
+  int age = 25;
+  double price = 19.99;
+  String name = "Olivier";
+  bool isStudent = true;
+
+  print(age);
+  print(price);
+  print(name);
+  print(isStudent);
+}
